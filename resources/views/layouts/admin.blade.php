1af9fd6dd87cd5d5de4cc2e52d93a9dd322226bb
vzdee <mario.mendoza716@gmail.com>
2026-02-20 22:11:38 -0600

    feat: implement layout with Flowbite and dynamic sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,139 +19,18 @@
     @livewireStyles
 </head>
 
-<body class="font-sans antialiased">
+<body class="font-sans antialiased bg-grey-20">
     @include('layouts.includes.admin.navigation');
     @include('layouts.includes.admin.sidebar');
     <div class="p-4 sm:ml-64 mt-14">
-        <div class="p-4 border-1 border-default border-dashed rounded-base">
-            <div class="grid grid-cols-3 gap-4 mb-4">
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-            </div>
-            <div class="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
-                <p class="text-fg-disabled">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                        fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 12h14m-7 7V5" />
-                    </svg>
-                </p>
-            </div>
-            <div class="grid grid-cols-2 gap-4 mb-4">
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-            </div>
-            <div class="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
-                <p class="text-fg-disabled">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                        fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 12h14m-7 7V5" />
-                    </svg>
-                </p>
-            </div>
-            <div class="grid grid-cols-2 gap-4">
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-                <div class="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                    <p class="text-fg-disabled">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                            height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </p>
-                </div>
-            </div>
+        <div class="mt-4">
+            {{$slot}}
         </div>
     </div>
     @stack('modals')
 
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
 </body>
 
 </html>
--- resources/views/layouts/includes/admin/navigation.blade.php
+++ resources/views/layouts/includes/admin/navigation.blade.php
@@ -11,55 +11,64 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10" />
           </svg>
         </button>
-        <a href="https://flowbite.com" class="flex ms-2 md:me-24">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="FlowBite Logo" />
-          <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="/" class="flex ms-2 md:me-24">
+          <img src="{{asset('img/logo_personal.svg')}}" class="h-10 me-2" alt="Logo"/>
+          <span class="self-center text-lg font-semibold whitespace-nowrap text-black">Medical Appointment</span>
         </a>
       </div>
       <div class="flex items-center">
-        <div class="flex items-center ms-3">
-          <div>
-            <button type="button"
-              class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-              aria-expanded="false" data-dropdown-toggle="dropdown-user">
-              <span class="sr-only">Open user menu</span>
-              <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                alt="user photo">
-            </button>
-          </div>
-          <div class="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44"
-            id="dropdown-user">
-            <div class="px-4 py-3 border-b border-default-medium" role="none">
-              <p class="text-sm font-medium text-heading" role="none">
-                Neil Sims
-              </p>
-              <p class="text-sm text-body truncate" role="none">
-                neil.sims@flowbite.com
-              </p>
-            </div>
-            <ul class="p-2 text-sm text-body font-medium" role="none">
-              <li>
-                <a href="#"
-                  class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                  role="menuitem">Dashboard</a>
-              </li>
-              <li>
-                <a href="#"
-                  class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                  role="menuitem">Settings</a>
-              </li>
-              <li>
-                <a href="#"
-                  class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                  role="menuitem">Earnings</a>
-              </li>
-              <li>
-                <a href="#"
-                  class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                  role="menuitem">Sign out</a>
-              </li>
-            </ul>
-          </div>
+        <div class="ms-3 relative">
+          <x-dropdown align="right" width="48">
+            <x-slot name="trigger">
+              @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                <button
+                  class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                  <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                    alt="{{ Auth::user()->name }}" />
+                </button>
+              @else
+                <span class="inline-flex rounded-md">
+                  <button type="button"
+                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                    {{ Auth::user()->name }}
+
+                    <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                      stroke-width="1.5" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                  </button>
+                </span>
+              @endif
+            </x-slot>
+
+            <x-slot name="content">
+              <!-- Account Management -->
+              <div class="block px-4 py-2 text-xs text-gray-400">
+                {{ __('Manage Account') }}
+              </div>
+
+              <x-dropdown-link href="{{ route('profile.show') }}">
+                {{ __('Profile') }}
+              </x-dropdown-link>
+
+              @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                  {{ __('API Tokens') }}
+                </x-dropdown-link>
+              @endif
+
+              <div class="border-t border-gray-200"></div>
+
+              <!-- Authentication -->
+              <form method="POST" action="{{ route('logout') }}" x-data>
+                @csrf
+
+                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                  {{ __('Log Out') }}
+                </x-dropdown-link>
+              </form>
+            </x-slot>
+          </x-dropdown>
         </div>
       </div>
     </div>
